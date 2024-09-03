389329274d960c14ecaf1a534705b8a9c78f6f17
Utkarsh Lanjewar <lanjewarutkarsh@gmail.com>
2024-09-03 14:37:10 +0530

    Added signin controller

--- src/controller/user.js
+++ src/controller/user.js
@@ -1,10 +1,10 @@
-const User = require('../models/user')
+const User = require('../models/User')
 const bcrypt = require("bcrypt");
 const ErrorHandler = require('../features/utility-class')
 
 const signUpUser = async(req,res,next) => {
     try {
-        let user = await User.findOn({ email: req.body.email });
+        let user = await User.findOne({ email: req.body.email });
 		if (user){
             //check stauts code
              return next(new ErrorHandler('User with given email already Exist!',401))
@@ -15,14 +15,31 @@ const signUpUser = async(req,res,next) => {
 
 		user = await new User({ ...req.body, password: hashPassword }).save();
 
-        res.status(200).json({ message: 'User Created Successfully' })
+        return res.status(200).json({ message: 'User Created Successfully' })
     } catch (error) {
         next(error)
     }
 }
 
-const signInUser = () => {
+const signInUser = async(req,res,next) => {
+    try {
+        const user = await User.findOne({ email: req.body.email });
+		if (!user){
+            return next(new ErrorHandler('Invalid Email or Password',401))
+        }
 
+		const validPassword = await bcrypt.compare(
+			req.body.password,
+			user.password
+		);
+		if (!validPassword){
+            return next(new ErrorHandler('Invalid Email or Password',401))
+        }
+        return res.status(200).json({ message: 'User SignedIn Successfully' })
+
+    } catch (error) {
+        next(error)
+    }
 }
 
 
